48b1c0a4c4aae6df6453f32a55c89d1c26db322d
yoongjin <45158252+yoongjin@users.noreply.github.com>
2019-02-10 06:09:54 +0800

    First Commit

--- app/src/main/java/com/example/jeanniesecure/MainActivity.java
+++ app/src/main/java/com/example/jeanniesecure/MainActivity.java
@@ -2,15 +2,24 @@ package com.example.jeanniesecure;
 
 
 import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +37,33 @@ public class MainActivity extends AppCompatActivity {
 
     private int STORAGE_PERMISSION_CODE = 1;
 
+    public String[] slide_desc = {
+            "The GPS Location service in Phone Sage need permission",
+            "We need this permission to read and write data when download file / backup or restore contacts and sms",
+            "We need this permission to backup and restore contacts",
+            "We need this permission when receive a call",
+            "We need this permission to backup and restore sms and send phone location to safe phone number",
+            "We need this permission to read call log or intercept call",
+    };
+
+    public String[] permissions = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.SYSTEM_ALERT_WINDOW,
+            Manifest.permission.READ_SMS,
+            Manifest.permission.READ_PHONE_STATE,
+    };
+
+    public int[] permissions_code = {
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,14 +79,19 @@ public class MainActivity extends AppCompatActivity {
 
         mSlideViewPager.setAdapter(sliderAdapter);
 
-
         addDotsIndicator(0);
         mSlideViewPager.addOnPageChangeListener(viewListener);
 
         mNextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                mSlideViewPager.setCurrentItem(mCurrentPage+1);
+                if (ContextCompat.checkSelfPermission( MainActivity.this,
+                        permissions[mCurrentPage]) == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(MainActivity.this, "You have already granted this permission!", Toast.LENGTH_SHORT).show();
+                    mSlideViewPager.setCurrentItem(mCurrentPage+1);
+                } else {
+                    requestStoragePermission(mCurrentPage);
+                }
             }
         });
 
@@ -60,7 +101,44 @@ public class MainActivity extends AppCompatActivity {
                 mSlideViewPager.setCurrentItem(mCurrentPage-1);
             }
         });
+
     }
+
+    private void requestStoragePermission(final int position) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[position])) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage(slide_desc[position])
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permissions[position]}, permissions_code[position]);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{permissions[position]}, permissions_code[position]);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == permissions_code[mCurrentPage]){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                mSlideViewPager.setCurrentItem(mCurrentPage+1);
+            } else{
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 
     public void addDotsIndicator(int position){
         mDots = new TextView[6];
@@ -122,7 +200,4 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
-    }
 }
--- app/src/main/java/com/example/jeanniesecure/SliderAdapter.java
+++ app/src/main/java/com/example/jeanniesecure/SliderAdapter.java
@@ -1,10 +1,16 @@
 package com.example.jeanniesecure;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.content.Context;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
@@ -12,17 +18,20 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class SliderAdapter extends PagerAdapter {
 
     Context context;
+    AppCompatActivity activity;
     LayoutInflater layoutInflater;
+    private int STORAGE_PERMISSION_CODE = 1;
 
     public SliderAdapter(Context context) {
-
         this.context= context;
+        this.activity = (AppCompatActivity) context;
     }
 
     //Arrays
@@ -55,13 +64,14 @@ public class SliderAdapter extends PagerAdapter {
     };
 
     public String[] permissions = {
-            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.READ_CONTACTS,
             Manifest.permission.SYSTEM_ALERT_WINDOW,
             Manifest.permission.READ_SMS,
             Manifest.permission.READ_PHONE_STATE,
     };
+
 
     @Override
     public int getCount() {
@@ -82,15 +92,64 @@ public class SliderAdapter extends PagerAdapter {
         TextView slideHeading = (TextView) view.findViewById(R.id.slide_header);
         TextView slideDescription = (TextView) view.findViewById(R.id.slide_desc);
 
+        ImageButton okButton = (ImageButton) view.findViewById(R.id.requestButton);
+
         slideImageView.setImageResource(slide_images[position]);
         slideHeading.setText(slide_headings[position]);
         slideDescription.setText(slide_desc[position]);
 
 
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission( context,
+                        permissions[position]) == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(context, "You have already granted this permission!", Toast.LENGTH_SHORT).show();
+                } else {
+                    requestStoragePermission(position);
+                }
+            }
+        });
+
         container.addView(view);
 
         return view;
     }
+
+    private void requestStoragePermission(final int position) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permissions[position])) {
+            new AlertDialog.Builder(context)
+                    .setTitle("Permission needed")
+                    .setMessage(slide_desc[position])
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(activity, new String[]{permissions[position]}, STORAGE_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(activity, new String[]{permissions[position]}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show();
+            } else{
+                Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 
     public void destroyItem(ViewGroup container, int position, Object object){
         container.removeView((LinearLayout)object);
